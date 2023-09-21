69ef49f22d2bc8fbf16bfd4eac196536d7539a88
nec <nec@activeviam.com>
2023-09-21 15:12:25 +0200

    Add no coalesce test case

--- src/main/java/com/activeviam/databricks/clustervswarehouse/Main.java
+++ src/main/java/com/activeviam/databricks/clustervswarehouse/Main.java
@@ -17,11 +17,17 @@ public class Main {
 	private static final String SCHEMA_NAME = "test_schema_debug";
 	private static final String TABLE_NAME = "difference_cluster_vs_warehouse";
 
-	private static final String SQL_QUERY =
+	private static final String SQL_QUERY_WITH_COALESCE =
 			"SELECT \n"
 			+ " ARRAY_AGG(DISTINCT COALESCE(`T0`.`date`, '1970-01-01')) AS `distinct_date`\n"
 			+ "FROM \n"
 			+ " `" + CATALOG_NAME + "`.`" + SCHEMA_NAME + "`.`" + TABLE_NAME + "` AS T0";
+
+	private static final String SQL_QUERY_NO_COALESCE =
+			"SELECT \n"
+					+ " ARRAY_AGG(DISTINCT `T0`.`date`) AS `distinct_date`\n"
+					+ "FROM \n"
+					+ " `" + CATALOG_NAME + "`.`" + SCHEMA_NAME + "`.`" + TABLE_NAME + "` AS T0";
 
 
 	private static final String SERVER_HOSTNAME = System.getenv("DATABRICKS_SERVER_HOSTNAME");
@@ -46,19 +52,24 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 
 		LOGGER.info("Performing test on cluster");
-		generateTestCase(CONNECTION_STRING_CLUSTER);
+		generateTestCase(CONNECTION_STRING_CLUSTER, SQL_QUERY_WITH_COALESCE);
+		generateTestCase(CONNECTION_STRING_CLUSTER, SQL_QUERY_NO_COALESCE);
 		LOGGER.info("Performing test on warehouse");
-		generateTestCase(CONNECTION_STRING_WAREHOUSE);
+		generateTestCase(CONNECTION_STRING_WAREHOUSE, SQL_QUERY_WITH_COALESCE);
+		generateTestCase(CONNECTION_STRING_WAREHOUSE, SQL_QUERY_NO_COALESCE);
 
 	}
 
-	private static void generateTestCase(final String connectionString) throws SQLException {
+	private static void generateTestCase(final String connectionString, final String query) throws SQLException {
+		LOGGER.info(() -> "Test for query " + query);
 		try (
 				final Connection connection = createConnection(connectionString);
-				final PreparedStatement statement = connection.prepareStatement(SQL_QUERY);
+				final PreparedStatement statement = connection.prepareStatement(query);
 				final ResultSet resultSet = statement.executeQuery()) {
 			while (resultSet.next()) {
-				System.out.println(resultSet.getObject("distinct_date"));
+				final Object rawResult = resultSet.getObject("distinct_date");
+				System.out.println(rawResult.getClass());
+				System.out.println(rawResult);
 			}
 		}
 	}
